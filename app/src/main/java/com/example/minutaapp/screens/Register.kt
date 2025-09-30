fcc8b7cb1109ea05ebb065575bd6d9682dda015a
Albania Musabeli <musabelialbania@gmail.com>
2025-09-30 15:03:26 -0300

    feat(registro): Agregar icono y funcionalidad para ver la contraseña

--- app/src/main/java/com/example/minutaapp/screens/Register.kt
+++ app/src/main/java/com/example/minutaapp/screens/Register.kt
@@ -1,22 +1,15 @@
 package com.example.minutaapp.screens
 
 import android.widget.Toast
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -29,14 +22,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.minutaapp.SimpleTopBar
-
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.text.input.VisualTransformation
+import com.example.minutaapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,6 +43,8 @@ fun RegisterScreen(onBack: () -> Unit) {
     var password by remember { mutableStateOf("") }
     var repeatPassword by remember { mutableStateOf("") }
     val context = LocalContext.current // hace referencia a la vista para mostrar el Toast
+    var isPasswordVisible by remember { mutableStateOf(false) }
+    var isRepeatPasswordVisible by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = { SimpleTopBar(title = "Registro", showBack = true, onBack = onBack) }
@@ -90,11 +89,20 @@ fun RegisterScreen(onBack: () -> Unit) {
                     onValueChange = { password = it },
                     label = { Text("Contraseña") },
                     singleLine = true,
-                    visualTransformation = PasswordVisualTransformation(),
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Password,
                         imeAction = ImeAction.Next
                     ),
+                    trailingIcon = {
+                        IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                            Image(
+                                painter = painterResource(id = if (isPasswordVisible) R.drawable.ic_visibility else R.drawable.ic_visibility_off),
+                                contentDescription = if (isPasswordVisible) "Ocultar contraseña" else "Mostrar contraseña",
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }
+                    },
                     modifier = Modifier.fillMaxWidth()
                 )
                 OutlinedTextField(
@@ -102,11 +110,20 @@ fun RegisterScreen(onBack: () -> Unit) {
                     onValueChange = { repeatPassword = it },
                     label = { Text("Repetir Contraseña") },
                     singleLine = true,
-                    visualTransformation = PasswordVisualTransformation(),
+                    visualTransformation = if (isRepeatPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Password,
                         imeAction = ImeAction.Done
                     ),
+                    trailingIcon = {
+                        IconButton(onClick = { isRepeatPasswordVisible = !isRepeatPasswordVisible }) {
+                            Image(
+                                painter = painterResource(id = if (isRepeatPasswordVisible) R.drawable.ic_visibility else R.drawable.ic_visibility_off),
+                                contentDescription = if (isRepeatPasswordVisible) "Ocultar contraseña" else "Mostrar contraseña",
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }
+                    },
                     modifier = Modifier.fillMaxWidth()
                 )
                 // Fila para organizar los botones horizontalmente
